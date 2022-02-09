2a057602be66aa954d991ae0c8c2af9b8f17bfd6
lawrence254 <lawrenslava1@gmail.com>
2022-02-09 13:43:12 +0300

    Complete tests

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 25;
+let num4 = 7;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 5, 4, 6, 20, 19, 12, 7, 9)
